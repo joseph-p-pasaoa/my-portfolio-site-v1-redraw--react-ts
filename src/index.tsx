5934d584fb3b985eb9407ca44c635aaf0cd2b73f
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-08-14 17:55:57 -0400

    remove boilerplate code, personalize base files

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
+/*
+JOSEPH P. PASAOA
+My Portfolio Site v1
+https://dev.josephpasaoa.com/
+*/
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import './index.scss';
+import Main from './Main';
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +22,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
