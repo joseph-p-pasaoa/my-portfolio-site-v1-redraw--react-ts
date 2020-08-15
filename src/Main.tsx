0a0ac1848331577f1f7032db9fb7ffcabbf03179
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-08-15 02:39:55 -0400

    add base global styles

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 import './Main.scss';
 
+
 export default function Main() {
   return (
     <div className="Main">
-      <header className="Main-header">
-        Joseph P. Pasaoa
+      <header className="">
+        Hello! My name is...<br />
+        Joseph P. Pasaoa<br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ipsam repudiandae recusandae maiores minima, ipsa ratione blanditiis aut. Incidunt veniam deleniti a fugiat nulla numquam reprehenderit vitae animi tenetur ipsa.</p>
       </header>
     </div>
   );
