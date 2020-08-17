1a39ad4a68d850d4d716e0f67b8d8f8532f02e6b
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-08-17 11:14:32 -0400

    init ProjectPage

--- src/Main.tsx
+++ src/Main.tsx
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import './Main.scss';
 import TopBar from './components/TopBar';
 import HomePage from './pages/HomePage';
+import ProjectsPage from './pages/ProjectsPage';
 
 
 export default function Main() {
@@ -12,10 +13,12 @@ export default function Main() {
          <figure className="background-dimmer"></figure>
 
          <div className="layout-base">
+            {/* ASIDE BARS (only one is "display: block" at any time) */}
             <TopBar />
 
+            {/* PAGE ROUTING */}
             <Switch>
-               {/* <Route path={`/projects`} component={ProjectsPage} /> */}
+               <Route path={`/projects`} component={ProjectsPage} />
                {/* <Route path={`/contactme`} component={ContactMePage} /> */}
                {/* <Route path={`/skills`} component={SkillsPage} /> */}
                {/* <Route path={`/blog`} component={BlogPage} /> */}
---
+++ src/pages/ProjectsPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const ProjectsPage = () => {
+  return (
+    <>ProjectsPage</>
+  );
+}
+
+
+export default ProjectsPage;
