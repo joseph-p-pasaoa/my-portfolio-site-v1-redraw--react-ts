0ace48bb2e292204c497ef5d53aad16d4200591f
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-08-17 23:47:48 -0400

    add working useOutsideAlerter implementation

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 
 import NameBox from './Bars/NameBox';
 import TopNavAccordion from './Bars/TopNavAccordion';
 import NavListItems from './Bars/NavListItems';
 
+/**
+ * Hook that alerts clicks outside of the passed ref
+ */
+function useOutsideAlerter(ref: any) {
+   useEffect(() => {
+      /**
+      * Alert if clicked on outside of element
+      */
+      function handleClickOutside(event: any) {
+         if (ref.current && !ref.current.contains(event.target)) {
+               alert("You clicked outside of me!");
+         }
+      }
+
+      // Bind the event listener
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+         // Unbind the event listener on clean up
+         document.removeEventListener("mousedown", handleClickOutside);
+      };
+   }, [ref]);
+}
+
 
 const TopBar = () => {
+   const wrapperRef = useRef<HTMLDivElement>(null);
+   useOutsideAlerter(wrapperRef);
+
    return(
-      <section className='topbar'>
+      <section className='topbar' ref={wrapperRef}>
          <figure className="topbar__background"></figure>
          <NameBox />
 
