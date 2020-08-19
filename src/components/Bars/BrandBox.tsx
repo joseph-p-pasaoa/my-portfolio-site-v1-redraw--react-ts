e3d398ee9b2cb6b3aaeab1483b505108900251b1
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-08-18 23:19:28 -0400

    add copy content to brandbox

--- src/components/Bars/BrandBox.tsx
+++ src/components/Bars/BrandBox.tsx
@@ -1,16 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Accordion from 'react-bootstrap/Accordion';
+import { useAccordionToggle } from 'react-bootstrap/AccordionToggle';
 
 
 const BrandBox = () => {
    const toggleNavCollapse = useAccordionToggle("");
 
    return (
-      <div className="namebox">
-         {/* <Link */}
-         <h1 className="namebox__name namebox__name--brand">
-            JP
-         </h1>
+      <div className="brandbox">
+
+         <Link to={`/`} className="brand-link" onClick={toggleNavCollapse}>
+
+            <div className="brand--home">
+               <h1>
+                  JP
+               </h1>
+               <h2>
+                  Welcome!
+               </h2>
+            </div>
+
+            <div className="brand--not-home">
+               <h1>
+                  Joseph P.<br />
+                  Pasaoa
+               </h1>
+               <h2>
+                  software engineer /<br />
+                  full-stack developer
+               </h2>
+            </div>
+
+         </Link>
+
+         <Accordion.Collapse eventKey="0">
+            <div>
+               IconsPH
+            </div>
+         </Accordion.Collapse>
+
       </div>
    );
 }
