dbd4c6821de7d9f51a77aa44eedd04385a281c8f
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-08-19 00:43:59 -0400

    build brandbox swap system using swapped opacities

--- src/components/Bars/BrandBox.tsx
+++ src/components/Bars/BrandBox.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Accordion from 'react-bootstrap/Accordion';
+import { Link, useRouteMatch } from 'react-router-dom';
 import { useAccordionToggle } from 'react-bootstrap/AccordionToggle';
 
 
 const BrandBox = () => {
+   const isAtHome = useRouteMatch({ exact: true, path: "/" });
    const toggleNavCollapse = useAccordionToggle("");
+
+   let brandStyle: BrandStyleObj = isAtHome
+      ?  { home: 'show', notHome: 'hide' }
+      :  { home: 'hide', notHome: 'show' };
 
    return (
       <div className="brandbox">
 
          <Link to={`/`} className="brand-link" onClick={toggleNavCollapse}>
 
-            <div className="brand--home">
+            <div className={`brand--home ${brandStyle['home']}`}>
                <h1>
                   JP
                </h1>
@@ -21,7 +25,7 @@ const BrandBox = () => {
                </h2>
             </div>
 
-            <div className="brand--not-home">
+            <div className={`brand--not-home ${brandStyle['notHome']}`}>
                <h1>
                   Joseph P.<br />
                   Pasaoa
