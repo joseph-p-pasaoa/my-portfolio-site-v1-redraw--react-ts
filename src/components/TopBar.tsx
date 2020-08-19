5cde7efa8d28c0d5b168ab87453b7c171f5cb699
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-08-18 22:27:25 -0400

    remove extra code, complete click outside of topbar collapses navaccordion

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -6,34 +6,7 @@ import NavHamburger from './Bars/NavHamburger';
 import NavListItems from './Bars/NavListItems';
 
 
-/**
- * Hook that alerts clicks outside of the passed ref
- */
-// function useOutsideAlerter(ref: any) {
-//    useEffect(() => {
-//       /**
-//       * Alert if clicked on outside of element
-//       */
-//       function handleClickOutside(event: any) {
-//          if (ref.current && !ref.current.contains(event.target)) {
-//                alert("You clicked outside of me!");
-//          }
-//       }
-
-//       // Bind the event listener
-//       document.addEventListener("mousedown", handleClickOutside);
-//       return () => {
-//          // Unbind the event listener on clean up
-//          document.removeEventListener("mousedown", handleClickOutside);
-//       };
-//    }, [ref]);
-// }
-
-
 const TopBar = () => {
-   // const wrapperRef = useRef<HTMLDivElement>(null);
-   // useOutsideAlerter(wrapperRef);
-
    return(
       <section className='topbar'>
          <figure className="topbar__background"></figure>
