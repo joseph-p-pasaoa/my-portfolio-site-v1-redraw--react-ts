9654fd860c2b58f7609dac3669c74d17f1f27d17
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-08-21 01:01:49 -0400

    add links and link styling to topbar icons

--- src/components/Bars/BrandIcons.tsx
+++ src/components/Bars/BrandIcons.tsx
@@ -5,8 +5,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const BrandIcons = () => {
    return (
       <div className="brand__icons">
-         <FontAwesomeIcon icon={["fab", "github-square"]} className="fontawesome--topnav" />
-         <FontAwesomeIcon icon={["fab", "linkedin"]} className="fontawesome--topnav" />
+         <a
+            href="https://github.com/joseph-p-pasaoa"
+            className="topbar__icon-link"
+            data-name="Github"
+            target="_blank"
+            rel="noopener noreferrer"
+         >
+            <FontAwesomeIcon icon={["fab", "github-square"]} className="fontawesome-icon--topnav" />
+         </a>
+         <a
+            href="https://www.linkedin.com/in/josephpasaoa/"
+            className="topbar__icon-link"
+            data-name="LinkedIn"
+            target="_blank"
+            rel="noopener noreferrer"
+         >
+            <FontAwesomeIcon icon={["fab", "linkedin"]} className="fontawesome-icon--topnav" />
+         </a>
       </div>
    );
 }
