ffbb8c3244e5edf4e2f08dfada69335920a67b9e
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-08-20 23:34:37 -0400

    add github and linkedin icons to topbar

--- src/components/Bars/BrandIcons.tsx
+++ src/components/Bars/BrandIcons.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Accordion from 'react-bootstrap/Accordion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 const BrandIcons = () => {
    return (
       <Accordion.Collapse eventKey="0">
-         <div>
-            IconsPH
+         <div className="brand__icons">
+            <FontAwesomeIcon icon={["fab", "github-square"]} className="icon-fontawesome" />
+            <FontAwesomeIcon icon={["fab", "linkedin"]} className="icon-fontawesome" />
          </div>
       </Accordion.Collapse>
    );
