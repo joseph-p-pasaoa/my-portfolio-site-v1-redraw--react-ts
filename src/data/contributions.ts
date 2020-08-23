aeef32d4aa04329463674233b1ffd324198f6eea
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-08-23 11:14:05 -0400

    init contributions log file

---
+++ src/data/contributions.ts
@@ -0,0 +1,16 @@
+export const contributions = {
+   /*
+   -- ICONS
+   <a href="https://iconscout.com/icons/typescript" target="_blank">Typescript Icon</a> by <a href="https://iconscout.com/contributors/icon-mafia">Icon Mafia</a> on <a href="https://iconscout.com">Iconscout</a>
+
+   -- BACKGROUND
+   Photo by <strong><a href="https://www.pexels.com/@mateusz-dach-99805?utm_content=attributionCopyText&amp;utm_medium=referral&amp;utm_source=pexels">Mateusz Dach</a></strong> from <strong><a href="https://www.pexels.com/photo/business-computer-connection-contemporary-450035/?utm_content=attributionCopyText&amp;utm_medium=referral&amp;utm_source=pexels">Pexels</a></strong></div>
+
+
+
+
+   */
+};
+
+
+export default contributions;
