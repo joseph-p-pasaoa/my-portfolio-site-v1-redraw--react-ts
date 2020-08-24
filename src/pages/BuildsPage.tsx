e61b9255ba33ec4d903ca3dc0beb677271dbc84b
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-08-23 23:54:58 -0400

    fix positioning of header on homepage, add page title to builds page

--- src/pages/BuildsPage.tsx
+++ src/pages/BuildsPage.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 const ProjectsPage = () => {
    return (
       <main>
+         <h4 className='page__title no-flow'>Builds</h4>
          ProjectsPage
       </main>
    );
