81a52197402d6bfddee3544b247e3045d8cb3beb
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-08-24 00:00:10 -0400

    connect projects data to projectsPage

--- src/pages/BuildsPage.tsx
+++ src/pages/BuildsPage.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
+import projects from '../data/projects';
+
 
 const ProjectsPage = () => {
+
+
+
    return (
       <main>
          <h4 className='page__title no-flow'>Builds</h4>
