4691aa8e58c37672d17a143507d8b646a3571069
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-08-23 23:48:55 -0400

    edit homepage content

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,24 +7,17 @@ const HomePage = () => {
          <div className='first-line'>
             <div className='greeting'>Hi!</div>
             <div className='pre-name'>I'm</div>
-            <h3 className='name'>
-               Joseph<br />
-               <span className='dim'>P. Pasaoa</span>
-            </h3>
          </div>
+         <h3 className='name'>
+            Joseph
+            <span className='dim'> P. Pasaoa</span>
+         </h3>
 
          <article>
-            <p className='article-copy'>I learned <strong>BASIC</strong> on my own when I was small. In college, I studied International Relations and Comparative Politics.</p>
-
-            <p className='article-copy'>I've worked hard as a First Assistant to a high-fashion photographer in NYC for a number of years. I've also been a professional dance, fashion, and portrait photographer, myself.</p>
-
-            <p className='article-copy'>Yet it wasn't until I was selected into <a href='https://www.pursuit.org/' target='_blank' rel='noopener noreferrer' className='link--homepursuit'>Pursuit</a> —an intensive, competitive <strong>full-stack</strong> coding Fellowship, that I discovered how much I truly enjoy programming and development.</p>
-
-            <p className='article-copy'>My experiences in the visual arts and political psychology have been a great well to draw from when developing front-ends and building <strong>effective UI/UX</strong> but I'm just as fascinated with the <strong>problem solving</strong>, <strong>optimizing</strong>, and <strong>design processes</strong> found all across the stack.</p>
-
-            <p className='article-copy'>I'm looking to find an <strong>rich and diverse environment</strong> where I can grow doubly as an engineer and as a <strong>teammate</strong>.</p>
-
-            <p className='article-copy'>Thank you for visiting. Please, have a fun look around!</p>
+            <p>I'm a full-stack developer specialized in the PERN stack (PostgreSQL, Express, React.js, Node.js) and based in New York City.</p>
+            <p>I like to build. I like to learn new things and make mistakes in the <a href='https://www.pursuit.org/' target='_blank' rel='noopener noreferrer' className='inline-link'>Pursuit</a> of being better.</p>
+            <p>Skilled with Typescript, Python, React Redux, SCSS/CSS, Jest, and HTML5. Adobe Creative Suite and Figma.</p>
+            <p>Please, have a fun look around. Thank you for visiting!</p>
          </article>
       </main>
    );
