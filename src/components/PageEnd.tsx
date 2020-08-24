3de423d681aca2397cb2b2166a8612c2d76ce4a3
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-08-23 22:49:08 -0400

    create working PageEnd component sensitive to location

--- src/components/PageEnd.tsx
+++ src/components/PageEnd.tsx
@@ -1,20 +1,43 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
 
-interface PageEndProps {
-   mode?: string;
+interface ButtonConditionals {
+   [key: string]: boolean;
+}
+
+const showReturnBtn: ButtonConditionals = {
+   '/skills': true,
+   '/builds': true
 }
 
 
-const PageEnd: React.FC<PageEndProps> = ({ mode }) => {
+const PageEnd = () => {
+   const {pathname} = useLocation();
+
+   const handleReturnToTop = () => {
+      const topAnchor: HTMLParagraphElement | null = document.querySelector('#pageTopAnchor');
+      if (topAnchor !== null) {
+         topAnchor.scrollIntoView({
+            behavior: 'smooth',
+            block: 'end',
+            inline: 'center'
+         });
+      }
+   }
+
+   let footerContent: JSX.Element | null = null;
+   if (showReturnBtn[pathname]) {
+      footerContent = <button
+         type='button'
+         className='top-return btn btn-info'
+         onClick={handleReturnToTop}
+      >Return to Top</button>;
+   }
+
+
    return (
       <footer>
-         <button
-            type='button'
-            className='top-return btn btn-info'
-            onClick={() => {
-               console.log('return to top');
-            }}
-         >Return to Top</button>
+         {footerContent}
       </footer>
    );
 }
