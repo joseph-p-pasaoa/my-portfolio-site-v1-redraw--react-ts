74c22fa11bc34b15bdd5082fd40e19c2ea13ed19
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-08-30 01:24:58 -0400

    complete buildsIndex buildImgs function

--- src/data/buildsIndex.ts
+++ src/data/buildsIndex.ts
@@ -1,4 +1,29 @@
-import endeavor from './builds/endeavor';
+import endeavorBase from './builds/endeavor';
+
+const path = require('path');
+const buildImagesPath = '/images/builds';
+
+const buildImgs = (buildObj: any) => {   // DEV
+   const { buildName, imgs } = buildObj;
+   const finalImgs = imgs.map((imgObj: any) => {   // DEV
+      const finalSrc = path.join(
+         buildImagesPath,
+         buildName,
+         imgObj.filename
+      );
+
+      return ({
+         caption: imgObj.caption,
+         filename: finalSrc
+      })
+   });
+
+   return {...buildObj, imgs: finalImgs};
+}
+
+const builds = [
+   endeavorBase,
+].map(base => buildImgs(base));
 
 
-export default [ endeavor ];
+export default builds;
