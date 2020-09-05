ddefb3f872eec588a0bf2fbfd29757967aae17be
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-09-04 23:57:32 -0400

    init BuildButtons component

---
+++ src/components/BuildsPage/BuildButtons.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface BuildButtonsProps {
+
+}
+
+
+const BuildButtons: React.FC<BuildButtonsProps> = ({}) => {
+   return (
+      <>
+      </>
+   );
+}
+
+
+export default BuildButtons;
